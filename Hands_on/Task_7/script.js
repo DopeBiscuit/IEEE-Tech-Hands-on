161c864c8a9fcaa494b0e39101e5e086fe537dc4
Abdelrahman Hany <boody753107@gmail.com>
2023-08-28 09:41:47 +0300

    Solved the questions

--- Hands_on/Task_7/script.js
+++ Hands_on/Task_7/script.js
@@ -1,182 +1,129 @@
-var a = 3;
-// a &&= 3;
-a ||= 2;
+
+var array = ['apple', 'strawberry', 'banana', 'orange', 'mango'];
+var select = document.getElementById('elements');
+populate();
+solve();
 
 
+function tabSwitch(param) {
+    // List of list items elements
+    var list = document.getElementsByClassName('nav-link');
 
-
-
-
-var c = null ;
-c ??=7;
-
-
-
-
-
-
-
-var num1 = 150_000_00;
-// console.log(num1);
-
-
-var x = 10;
-var myPromise1 = new Promise(function(resolve, reject){
-    if(x == 0){
-        resolve("Succesfullyyy done");
-    }else{
-        reject("7asal Error");
+    // resetting all active items, then activating clicked item.
+    for (var i = 0; i < list.length; i++) {
+        list[i].classList.remove('active');
     }
-})
+    param.classList.add('active');
 
+    // Display corresponding div body and hiding others.
+    if (param.id == 't1') {
+        document.getElementById('task1').classList.remove('d-none');
+        document.getElementById('task2').classList.add('d-none');
+        // Call task1 function.
+        populate();
+    }
+    else {
+        document.getElementById('task1').classList.add('d-none');
+        document.getElementById('task2').classList.remove('d-none');
+    }
+}
 
+function addOption(name, index) {
+    var option = document.createElement('option');
+    option.value = index;
+    option.innerHTML = name;
+    return option;
+}
 
+function populate() {
+    // Clear current elements
+    select.innerHTML = '<option selected value="none" disabled class="text-muted">Select Element</option>';
 
+    // Add all elements
+    for (var i = 0; i < array.length; i++) {
+        select.appendChild(addOption(array[i], i));
+    }
+    solve();
+}
 
+function popper() {
+    if(select.value != 'none')
+    {
+        array.splice(select.value, 1);
+        populate(select);
+    }
+}
 
+function adder() {
+    array.push(document.getElementById('txtelm').value);
+}
 
-// myPromise1.then(function(data){
-//     console.log(data);
-// }).catch(function(err){
-//     console.log(err);
-// })
+////////////////                            Solver part of the JS code
 
-class Person{
-    // private name;
-    // private age;
-    // public Person(name,age){
-    //     this.name = name;
-    //     this.age = age;
-    // }
-    constructor(name="default name",age=30){
-        this.name = name;
-        this.age = age;
+function solve () {
+    var q1 = array.every(item => !parseInt(item));
+    var q2 = array.some(item => item[0] == 'a');
+    var q3 = array.map( item => "I like  " + item);
+
+    console.log("trial");
+
+    que1(q1);
+    que2(q2);
+    que3(q3);
+}
+
+function que1 (ans) {
+    var p = document.getElementById('q1p');
+    var s = document.getElementById('q1s');
+
+    var imposter = 0;
+    for(var i = 0;i < array.length;i++) {
+        if(parseInt(array[i])) {
+            imposter += 1;
+        }
     }
 
-    test(){
-        console.log("Hello I'm working");
+    if(ans) {
+        p.innerHTML = "YEP, all your elements are strings.";
+        s.innerHTML = '';
+    }
+    else {
+        p.innerHTML = "There is an imposter among us.";
+        s.innerHTML = `There are exactly ${imposter} imposter(s) hiding. 🤫`;
     }
 
 }
 
+function que2 (ans) {
+    var p = document.getElementById('q2p');
+    var s = document.getElementById('q2s');
 
+    var lovers = 0;
 
-
-
-
-
-
-// class employee extends Person{
-//     constructor(name,age,salary){
-//         super(name,age);
-//         this.salary = salary;
-//     }
-
-// }
-// var person  = new employee ("ahmed",20,20000);
-// person.test();
-
-    
-    var arr = [2,3,4,20,5]
-
-    //find
-
-
-    // num=>num == 20 // function(num){ returns num == 20}
-    // console.log(arr.find(function(num,index){
-    //     console.log(num);
-    //     return num == 20;
-    // }));
-
-    // console.log (
-    //     arr.find((num)=>{return num == 200})
-    // )//undefined
-    //every [All Values At Array does it Equal To Num???]
-    // console.log (
-    //     arr.every( num=>num == 20 )
-    // )
-    // console.log (
-    //     arr2.every( num=>num == 5 )
-    // )//true
-    //some [if there exist at least one item???]
-    // console.log (
-    //     arr.some( num=>num == 20 )
-    // )//true
-    // console.log (
-    //     arr.some( num=>num == 2 )
-    // )//false
-    
-    //Filter
-    // console.log (
-    //     arr.filter( num=>num > 4 ) 
-    // )//[20]
-    // console.log(arr);
-    // console.log (
-    //     arr.filter( num=>num % 2 == 0 )
-    // )//[20]
-
-    // console.log (
-    //     )//[20]
-    //     arr.forEach( num=> console.log(num)  )
-
-
-
-    // console.log (
-    //     arr.map( num=> "this is num "+num  )
-    // )//[20]
-
-   
-
-    // var object = {firstname:"abdelrhman",age:22,blalbla:"blaala"};
-    // var object2 = {firstname:"ahmwed",age:22,blalbla:"blaala"};
-    // var object3 = {firstname:"hossam",age:22,blalbla:"blaala"};
-
-    // var arrayOfObject = {object,object2,object3}
-
-
-    // console.log(arrayOfObject);
-
-    var object = {username:"bedobeco",age:20,department:"IT",
-    interests:{
-        firstinterest: "programming",
-        secondinterest: "gaming"
-    }
-    }
-    console.log(object);
-
-    function GetData(){
-
-        var xhr = new XMLHttpRequest();
-        
-        //2) Connect [Open Connection]
-        xhr.open("GET","https://jsonplaceholder.typicode.com/users");
-        
-        //3)send
-        xhr.send('');
-        
-        //4) Listner [onreadystatechange]
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState == 4){
-                if(xhr.status == 200){ //404
-                    // console.log( JSON.parse(xhr.responseText) );
-                    // console.log(xhr.responseText);
-
-                    var Users = JSON.parse(xhr.responseText);//[{},{},{}]
-                    console.log(Users);
-                    var myDiv = document.getElementById("div");
-                    for(let i = 0; i<Users.length;i++){
-                        
-                        myDiv.innerHTML += 
-                        "<h1>"+Users[i].name+" Lives at "+Users[i].address.street+", "+Users[i].address.city+"</h1>"
-                    }
-                }
-            }
+    for(var i = 0;i < array.length;i++) {
+        if(array[i][0] == 'a') {
+            lovers += 1;
         }
-
-
-
-        //#endregion
     }
-    GetData();
 
-    
+    if(ans) {
+        p.innerHTML = "Looks like you love my boy 'a'.😁";
+        s.innerHTML = `${lovers} items start with the letter 'a'`;
+    }
+    else {
+        p.innerHTML = "Wow, you don't have to be this harsh!";
+        s.innerHTML = "Not a single item starts with the letter 'a'";
+    }
+
+}
+
+function que3 (ans) {
+    var s = document.getElementById('q3s');
+    var string = '';
+
+    q3.array.forEach(element => {
+        string += element + '<br>';
+    });
+
+    s.innerHTML = string;
+}
